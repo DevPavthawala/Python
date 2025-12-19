aa3c45f5a580930bf4ac4d1447d1c467c86416b2
DevPavthawala <devpavthawala6181@gmail.com>
2025-12-19 16:09:22 +0100

    for loops

---
+++ 17_for_loop.py
@@ -0,0 +1,24 @@
+# name = 'dev'
+
+# for i in name:
+#     print(i)
+#     if(i=='e'):
+#         print("This is special")
+
+# # for list
+# color = ["red", "green", 'blau','yellow']
+# for colors in color:
+#     print(colors)
+#     for i in colors:
+#         print(i)
+
+# range function
+
+# for k in range(5):
+#     print("From 0 to 4",k)
+
+# for k in range(1,10):
+#     print("From 1 to 5",k)
+
+for k in range(1,10,2):
+    print("From 1 to 5",k)
