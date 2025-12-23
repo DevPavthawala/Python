1307ba69d1d83e089d3e919e1fe3a558122257b8
DevPavthawala <devpavthawala6181@gmail.com>
2025-12-23 14:53:46 +0100

    understanding while loop

---
+++ 18_whilel_loop.py
@@ -0,0 +1,26 @@
+# i = int(input("Enter a no: "))
+
+# while(i<=3):
+#     i = int(input("Enter a no: "))
+#     print(i)
+
+
+# decrement loop*********************************
+
+# count = 10
+
+# while(count>0):
+#     print(count)
+#     count = count - 1
+
+# there is no do-while loop in python it is in c,c++,java
+
+i = 0
+while True:
+    print(i)
+    i = i+1
+    if(i%105 == 0):
+        break
+    
+    
+    
