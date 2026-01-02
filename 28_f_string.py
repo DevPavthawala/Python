76d4e998a3abb5ca033e21bc1e25bbb53b65f3ca
DevPavthawala <devpavthawala6181@gmail.com>
2026-01-02 16:13:02 +0100

    New learning

---
+++ 28_f_string.py
@@ -0,0 +1,12 @@
+letter = "Hey my name is {1} and I am form {0}"
+country = "India"
+name = "Dev"
+
+print(letter.format(country,name))
+
+print(f"Hey my name is {name}, I am from {country}")
+
+price = 4.235689741
+print(f"price is {price:.3f}")
+
+print(f"Hey my name is {{dev}} i am form india")
